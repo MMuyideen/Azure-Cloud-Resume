5dfd60ae8c3e2f9c1093eaf329cf1c9ef9f82ce3
Muyideen <dejimorenigbade@gmail.com>
2024-01-06 11:48:31 +0100

    Creating the Function App Locally

---
+++ backend/api/getresumecounter.cs
@@ -0,0 +1,44 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Collections.Concurrent;
+using System.Text;
+using System.Threading;
+
+
+namespace Company.Function
+{
+    public static class GetResumeCounter
+    {
+        [FunctionName("getresumecounter")]
+        public static HttpResponseMessage Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [CosmosDB(databaseName:"DeenAzureResume", containerName: "counter", Connection = "deenresumeconnectionstring", Id = "1", PartitionKey = "1")] Counter counter, 
+            [CosmosDB(databaseName:"DeenAzureResume", containerName: "counter", Connection = "deenresumeconnectionstring", Id = "1", PartitionKey = "1")] out Counter updatedCounter,
+            ILogger log)
+        {
+            // here is Where the counter gets updated
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            counter ??= new Counter();
+
+            updatedCounter = counter;
+            updatedCounter.Count += 1;
+
+            var jsonToReturn = JsonConvert.SerializeObject(counter);
+
+
+            return new HttpResponseMessage(System.Net.HttpStatusCode.OK)
+            {
+                Content = new StringContent(jsonToReturn, Encoding.UTF8, "application/json")
+            };
+        }
+    }
+}
